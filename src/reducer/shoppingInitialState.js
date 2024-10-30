8dd0ce1040b4633e1e4dde0248ed9a8b9c4fd9d8
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 20:56:31 -0300

    Crea estado del componente reducer shoppingInitialState.js

---
+++ src/reducer/shoppingInitialState.js
@@ -0,0 +1,10 @@
+export const shoppingInitialState = {
+    products: [
+        { id: 1, name: "Producto A", price: 10 },
+        { id: 2, name: "Producto B", price: 50 },
+        { id: 3, name: "Producto C", price: 120 },
+        { id: 4, name: "Producto D", price: 1540 },
+        { id: 5, name: "Producto E", price: 100 },
+    ],
+    cart: [],
+};
