b138d09c61a7ec4a4c040fb1b1c3979b27e6ec31
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 21:05:36 -0300

    Insertar compontente CardItem.js en carpeta molecules

---
+++ src/components/molecules/CardItem.js
@@ -0,0 +1,15 @@
+const CartItem = ({item, deleteFromCart}) => {
+
+    const {id, name, price, quantity} = item;
+ 
+   return (
+   <div className="cart-item">
+     <h4 className="cart-item-name">{name}</h4>
+     <h5 className="cart-item-price">$ {price} x {quantity} = {price * quantity}</h5>
+     <button onClick={() => deleteFromCart(id)} className="cart-item-buttons">Eliminar uno</button>
+     <button onClick={() => deleteFromCart(id, true)} className="cart-item-buttons">Eliminar todos</button>
+   </div>
+   );
+ };
+ 
+ export default CartItem
