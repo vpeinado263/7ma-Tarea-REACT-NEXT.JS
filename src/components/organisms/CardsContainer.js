5283371d6f06563310c2e191f890df2d741ca486
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 21:07:02 -0300

    Insertar componente CardsContainer

---
+++ src/components/organisms/CardsContainer.js
@@ -0,0 +1,32 @@
+import Card from "../components/Card";
+
+
+const productos = [
+    {
+        id: 1,
+        img: "https://http2.mlstatic.com/D_NQ_NP_716492-MLA52853049595_122022-O.webp",
+        title:  "Colchones de aire o espuma",
+        text: "Para reducir la presión en las áreas vulnerables.",
+        bgcolor: "green",
+    },
+  ];
+
+const CardContainer = () => {
+    return (
+    <>
+        <div>
+            {
+             productos.map(producto => <Card producto={producto}/>)
+            }
+        </div>
+        <style jsx>{`
+        div {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+        `}</style>
+    </>
+    )
+}
+export default CardContainer;
