e59485042b307905896916a809b09a1048a44edd
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 22:40:02 -0300

    Agregar estilos al componente Product.js

--- src/components/organisms/Product.js
+++ src/components/organisms/Product.js
@@ -1,19 +1,17 @@
+import styles from "@/styles/Product.module.css";
 import CardContainer from "./CardsContainer";
 
+const Product = ({ product, addToCart }) => {
+    const { id, name, price } = product;
 
+    return (
+        <div className={styles.product}>
+            <h4 className={styles.productName}>{name}</h4>
+            <h5 className={styles.productPrice}>$ {price}</h5>
+            <CardContainer />
+            <button onClick={() => addToCart(id)} className={styles.addToCart}>Agregar</button>
+        </div>
+    );
+};
 
-const Product = ({product, addToCart}) => {
-
-    const {id, name, price} = product;
- 
-   return (
-   <div className="product">
-     <h4 className="product-name">{name}</h4>
-     <h5 className="product-price">$ {price}</h5>
-     <CardContainer/>
-     <button onClick={() => addToCart(id)} className="add-to-cart">Agregar</button>
-   </div>
-   )
- }
- 
- export default Product;
+export default Product;
