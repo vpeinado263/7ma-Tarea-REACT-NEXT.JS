126d7295ec98f433832b7b79828b823e39e613ed
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 21:08:29 -0300

    Insertar componente de Product.js

---
+++ src/components/organisms/Product.js
@@ -0,0 +1,18 @@
+import CardContainer from "./CardsContainer";
+
+
+const Product = ({product, addToCart}) => {
+
+    const {id, name, price} = product;
+ 
+   return (
+   <div className="product">
+     <h4 className="product-name">{name}</h4>
+     <h5 className="product-price">$ {price}</h5>
+     <CardContainer/>
+     <button onClick={() => addToCart(id)} className="add-to-cart">Agregar</button>
+   </div>
+   )
+ }
+ 
+ export default Product;
