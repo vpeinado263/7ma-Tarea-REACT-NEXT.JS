c445357e8fadcae32275c628f3a69167dd124bc9
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 21:04:17 -0300

    Crear componente Card.js

---
+++ src/components/molecules/Card.js
@@ -0,0 +1,41 @@
+import Button from "../components/Button";
+
+
+const Card = ({producto}) => {
+    const {img,title, text, bgcolor, ccolor} = producto;
+
+    return(
+        <>
+        <figure>
+            <img src={img} alt={title}/>
+            <figcaption>
+                <h3>{title}</h3>
+            </figcaption>
+            <p>{text}</p>
+            <Button bgcolor={bgcolor} ccolor={ccolor}/>
+        </figure>
+        <style jsx>{`
+        figure {
+            width: auto;
+            height: auto;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        img {
+            width: 100%;
+            height: 40%;
+        }
+        h3 {
+            font-size: 1rem;
+            text-aling: center;
+        }
+        p{
+            font-size: 1rem;
+            text-aling: center;
+        }
+        `}</style>
+        </>
+    )
+}
+export default Card
