9f0fea192d2a099c9d79f24ecbc825ddd04d027c
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 22:14:04 -0300

    Realizar estilizacion de compontente de ShoppingCart.js

--- src/components/templates/ShoppingCart.js
+++ src/components/templates/ShoppingCart.js
@@ -2,45 +2,42 @@ import { useReducer } from "react";
 import { TYPES } from "../../actions/actions";
 import { shoppingReducer } from "../../reducer/shoppingReducer";
 import { shoppingInitialState } from "../../reducer/shoppingInitialState";
+import styles from "@/styles/ShoppingCart.module.css";
 import Product from "../organisms/Product";
 import CartItem from "../molecules/CardItem";
 
 const ShoppingCart = () => {
- 
   const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
-
-  // Destructurar el estado
   const { products, cart } = state;
 
   const addToCart = (id) => dispatch({ type: TYPES.ADD_TO_CART, payload: id });
 
   const deleteFromCart = (id, all = false) => {
     if (all) {
-      dispatch({type: TYPES.REMOVE_ALL_PRODUCTS, payload: id})
+      dispatch({ type: TYPES.REMOVE_ALL_PRODUCTS, payload: id });
     } else {
-    dispatch({type: TYPES.REMOVE_ONE_PRODUCT, payload: id})
+      dispatch({ type: TYPES.REMOVE_ONE_PRODUCT, payload: id });
     }
   };
 
-  // Mover la función clearCart dentro del componente
   const clearCart = () => dispatch({ type: TYPES.CLEAR_CART });
 
   return (
     <>
-      <h2 className="shopping-cart-title">Carrito de Compras</h2>
-      <h3 className="products-title">Productos</h3>
-      <div className="box grid-responsive">
-        {
-          products.map(product => <Product key={product.id} product={product} addToCart={addToCart} />)
-        }
+      <h2 className={styles.shoppingTitle}>Carrito de Compras</h2>
+      <h3 className={styles.productsTitle}>Productos</h3>
+      <div className={styles.gridResponsive}>
+        {products.map((product) => (
+          <Product key={product.id} product={product} addToCart={addToCart} />
+        ))}
       </div>
-      <h3 className="cart-title">Carrito</h3>
-      <div className="box">
-        {
-          cart.map((item, i) => <CartItem key={i} item={item} deleteFromCart={deleteFromCart} />)
-        }
+      <h3 className={styles.shoppingTitle}>Carrito</h3>
+      <div className={styles.box}>
+        {cart.map((item) => (
+          <CartItem key={item.id} item={item} deleteFromCart={deleteFromCart} />
+        ))}
       </div>
-      <button className="clear-cart-button" onClick={clearCart}>Limpiar carrito</button>
+      <button className={styles.clearButton} onClick={clearCart}>Limpiar carrito</button>
     </>
   );
 };
