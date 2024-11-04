1186aa5bc41885bb9b632b17ab5cb2eec6d26fb2
Victor Peinado <vpeinado263@gmail.com>
2024-11-04 11:25:25 -0300

    Finalizar estilos y logica de carro de compras

--- src/components/organisms/CardsContainer.js
+++ src/components/organisms/CardsContainer.js
@@ -6,18 +6,26 @@ const productos = shoppingInitialState.products;
 
     return (
     <>
-        <div>
+        <div className="cardContainer">
+            <div className="card">
             {
              productos.map(producto => <Card key={producto.id} producto={producto} addToCart={addToCart}/>)
             }
+            </div>
         </div>
 
         <style jsx>{`
-        div {
+        .cardContainer {
             display: flex;
             flex-wrap: wrap;
             justify-content: space-between;
+            gap: 16px; 
+            padding: 16px; 
         }
+        .cardContainer > :global(.card) {
+            flex: 1 1 calc(33.33% - 16px); 
+            max-width: 200px;
+          }
         `}</style>
     </>
     )
